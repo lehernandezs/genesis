95a66c3079721c06597e30db955cd7cdc3a49aea
lehernandezs <163459317+lehernandezs@users.noreply.github.com>
2024-04-09 14:34:49 -0500

    Primer commit

---
+++ src/main/java/co/edu/ufpso/genesis/apps/core/controllers/TablaController.java
@@ -0,0 +1,11 @@
+package co.edu.ufpso.genesis.apps.core.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TablaController {
+
+    public String prueba() {
+
+    }
+}
