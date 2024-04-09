192596180b0cd6a0c8d13077f7c4576013df0240
lehernandezs <163459317+lehernandezs@users.noreply.github.com>
2024-04-09 14:34:17 -0500

    Primer commit

---
+++ src/test/java/co/edu/ufpso/genesis/GenesisApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufpso.genesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
